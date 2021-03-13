308dfa2b2a8b8b95392f72ae87ec36aed8982820
tonsojun <josephjun@gmail.com>
2021-03-13 14:20:32 -0800

    Completed intro 9 ruby intro

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def main
   end
+
+  def add
+    new_params = {name: "", hobby: ""}
+    new_params[:name] = params[:name]
+    new_params[:hobby] = params[:hobby]
+    session[:students] = new_params
+    redirect_to "/"
+  end
 end
